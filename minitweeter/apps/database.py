06045674e1bf5949017bf6bfff570e14657c8144
jmpark <gabriel6846@gmail.com>
2014-07-29 20:29:29 +0900

    deleted

--- minitweeter/apps/database.py
+++
@@ -1,7 +0,0 @@
-class Database(object):
-    def __init__(self):
-        self.database = []
-    def put(self, storage):
-        self.database.append(storage)
-    def out(self):
-        return self.database
