3293be9eeb1ba7a59be70124a2423710727e7c4e
Park <gabriel6846@gmail.com>
2014-09-15 23:46:51 +0900

    회원가입 만들고 db랑 연결확인하였습니다. html파일은 우선 제가 숙제하면서 만든거 썼습니다.

--- apps/controllers.py
+++ apps/controllers.py
@@ -6,101 +6,38 @@ from sqlalchemy import desc
 from apps import app, db
 from apps.forms import JoinForm
 from apps.models import (
-    User
+     User
+#     Video,
+#     Event,
+#     Music,
+#     Portfolio
 )
-import pusher
 
-#
+
+
 # @index 
-#
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if 'user_email' in session :
+    return render_template("login.html")
 
-        return render_template("chat.html")        
-    return render_template("login.html",message="")
-
-@app.route('/send', methods=['GET'])    
-def sendmsg():
-    p = pusher.Pusher(
-        app_id='86616',
-        key='69ea7e9cd25b0aa6ff1c',
-        secret='6e11a0924988a1f07d57'
-    )
-
-    
-    chat_msg = request.args.get('msg_data')
-    p['test_channel'].trigger('event_msg', {'name': session['user_name'], 'msg' : chat_msg})
-    return ""
-
-
-# @Join controllers
-
-@app.route('/user/join/', methods=['GET', 'POST'])
+@app.route('/user/join/', methods=['GET','POST'])
 def user_join():
-    form = JoinForm()   
+    form = JoinForm()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         if form.validate_on_submit():
             user = User(
-                email=form.email.data,
-                password=generate_password_hash(form.password.data),
-                name=form.name.data
+                email = form.email.data,
+                password = generate_password_hash(form.password.data),
+                name = form.name.data
             )
-
             db.session.add(user)
             db.session.commit()
-
+            
             return redirect(url_for('index'))
-    else:
+    else :
         return render_template('/user/join.html', form=form)
-
-
-@app.route('/login/', methods=['POST'])
-def login():
-
-    email = request.form['email']
-    pw = request.form['pw']
-
-    user = User.query.get(email)
-    message = None
-
-    if user is None:
-        message = u"user가 존재하지 않습니다."
-    elif not check_password_hash(user.password,pw):
-        message = u"password가 잘못되었습니다."
-    else:
-        p = pusher.Pusher(
-            app_id='86616',
-            key='69ea7e9cd25b0aa6ff1c',
-            secret='6e11a0924988a1f07d57'
-        )
-
-        session.permanent = True
-        session['user_email'] = user.email
-        session['user_name'] = user.name
-
-        # 로그인 메세지 뿌리기
-        p['test_channel'].trigger('login_msg', {'name': user.name})
-        return redirect(url_for('index'))
-
-    return render_template("login.html",message=message)
-
-
-
-@app.route('/logout')
-def logout():
-    p = pusher.Pusher(
-        app_id='86616',
-        key='69ea7e9cd25b0aa6ff1c',
-        secret='6e11a0924988a1f07d57'
-    )
-    name = session['user_name']
-    session.clear()
-    
-    # 로그아웃 메세지 뿌리기
-    p['test_channel'].trigger('logout_msg', {'name': name})
-    return redirect(url_for('index'))
 
 #
 # @error Handlers
--- apps/models.py
+++ apps/models.py
@@ -4,72 +4,70 @@ models.py
 """
 from apps import db
 
-#
-# add User Model
-#
+
 class User(db.Model):
     email = db.Column(db.String(255), primary_key = True)
     password = db.Column(db.String(255))
     name = db.Column(db.String(255))
-    join_date = db.Column(db.DateTime(), default = db.func.now())
     page_domain = db.Column(db.String(255))
     subscribe = db.Column(db.Integer)
 
+    join_date = db.Column(db.DateTime(), default = db.func.now())
 
 
-    # 일대다 관계를 가진 비디오, 음원 등을 관계 설정
-    video = db.relationship('Video', backref="user", lazy='dynamic')
-    event = db.relationship('Event', backref="user", lazy='dynamic')
-    music = db.relationship('Music', backref="user", lazy='dynamic')
+#     # relationships to models 
+#     video = db.relationship('Video', backref="user", lazy='dynamic')
+#     event = db.relationship('Event', backref="user", lazy='dynamic')
+#     music = db.relationship('Music', backref="user", lazy='dynamic')
 
 
-class Video(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	title = db.Column(db.String(255))
-	artist = db.Column(db.String(255))
-	url = db.Column(db.String(255))
-	comments = db.Column(db.String(255))
-	User_email = db.Column(db.String(255), db.ForeignKey('user.email'))
+# class Video(db.Model):
+# 	id = db.Column(db.Integer, primary_key = True)
+# 	title = db.Column(db.String(255))
+# 	artist = db.Column(db.String(255))
+# 	url = db.Column(db.String(255))
+# 	comments = db.Column(db.String(255))
+# 	User_email = db.Column(db.String(255), db.ForeignKey('user.email'))
 
 
-class Event(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	title = db.Column(db.String(255))
-	comments = db.Column(db.String(255))
-	# img
-	user_email = db.Column(db.String(255), db.ForeignKey('user.email'))	
+# class Event(db.Model):
+# 	id = db.Column(db.Integer, primary_key = True)
+# 	title = db.Column(db.String(255))
+# 	comments = db.Column(db.String(255))
+# 	# img
+# 	user_email = db.Column(db.String(255), db.ForeignKey('user.email'))	
 
-class Music(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	title = db.Column(db.String(255))
-	artist = db.Column(db.String(255))
-	url = db.Column(db.String(255))
-	album = db.Column(db.String(255))
-	comments = db.Column(db.String(255))
-	user_email = db.Column(db.String(255), db.ForeignKey('user.email'))	
+# class Music(db.Model):
+# 	id = db.Column(db.Integer, primary_key = True)
+# 	title = db.Column(db.String(255))
+# 	artist = db.Column(db.String(255))
+# 	url = db.Column(db.String(255))
+# 	album = db.Column(db.String(255))
+# 	comments = db.Column(db.String(255))
+# 	user_email = db.Column(db.String(255), db.ForeignKey('user.email'))	
 
-class Portfolio(db.Model):
-	portfolio_domain = db.Column(db.String(255))
-	careers = db.Column(db.String(1000))
+# class Portfolio(db.Model):
+# 	portfolio_domain = db.Column(db.String(255))
+# 	careers = db.Column(db.String(1000))
 
-	# 각 항목들에 대한 ID
-	video1_id = db.Column(db.Integer)
-	video2_id = db.Column(db.Integer)
-	video3_id = db.Column(db.Integer)
-	video4_id = db.Column(db.Integer)
-	video5_id = db.Column(db.Integer)
+# 	# IDs to columns
+# 	video1_id = db.Column(db.Integer)
+# 	video2_id = db.Column(db.Integer)
+# 	video3_id = db.Column(db.Integer)
+# 	video4_id = db.Column(db.Integer)
+# 	video5_id = db.Column(db.Integer)
 
-	music1_id = db.Column(db.Integer)
-	music2_id = db.Column(db.Integer)
-	music3_id = db.Column(db.Integer)
-	music4_id = db.Column(db.Integer)
-	music5_id = db.Column(db.Integer)
+# 	music1_id = db.Column(db.Integer)
+# 	music2_id = db.Column(db.Integer)
+# 	music3_id = db.Column(db.Integer)
+# 	music4_id = db.Column(db.Integer)
+# 	music5_id = db.Column(db.Integer)
 
-	event1_id = db.Column(db.Integer)
-	event2_id = db.Column(db.Integer)
-	event3_id = db.Column(db.Integer)
-	event4_id = db.Column(db.Integer)
-	event5_id = db.Column(db.Integer)
+# 	event1_id = db.Column(db.Integer)
+# 	event2_id = db.Column(db.Integer)
+# 	event3_id = db.Column(db.Integer)
+# 	event4_id = db.Column(db.Integer)
+# 	event5_id = db.Column(db.Integer)
 
-	User_email = db.Column(db.String(255), db.ForeignKey('user.email'))	
+# 	User_email = db.Column(db.String(255), db.ForeignKey('user.email'))	
 	
